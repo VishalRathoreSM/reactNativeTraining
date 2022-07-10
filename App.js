9a25425f3da646cdb2a063a9cddeee53567fd8c6
Vishal Rathore <vishal.rathore@snackmagic.com>
2022-07-10 21:41:10 +0530

    second upload

--- App.js
+++ App.js
@@ -11,15 +11,15 @@ import React from 'react';
 import {store} from './src/store';
 import {Provider} from 'react-redux';
 // import BookForm from './src/components/library-app-2/book-form';
-// import OTP from './src/components/otp-exercise/otp';
+import OTP from './src/components/otp-exercise/otp';
 // import PlayingWithFonts from './src/components/playing-with-fonts-exercise/playing-with-fonts';
 
 const App = () => {
   return (
     <Provider store={store}>
-      {/* <BookForm />
-      {/* <OTP />
-      <PlayingWithFonts /> */}
+      {/* <BookForm /> */}
+      {/* <OTP /> */}
+      {/* <PlayingWithFonts /> */}
     </Provider>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import {configureStore} from '@reduxjs/toolkit';
+import reducer from './reducers';
 
 export const store = configureStore({
-  reducer: {},
+  reducer,
 });
